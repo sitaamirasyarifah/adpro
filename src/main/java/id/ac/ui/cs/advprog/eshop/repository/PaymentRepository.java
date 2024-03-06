519c8f3c1a61eb8e6f10460a443b22b855119e0e
sitaamirasyarifah <sitaamira86@gmail.com>
2024-03-06 20:13:54 +0700

    payment repository

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,45 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import java.util.*;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PaymentRepository {
+    private List<Payment> paymentData = new ArrayList<>();
+
+    public Payment save(Payment payment){
+        int i = 0;
+        for(Payment savedPayment : paymentData){
+            if(savedPayment.getId().equals(payment.getId())){
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i++;
+        }
+        paymentData.add(payment);
+        return payment;
+    }
+
+    public List<Payment> findAll() {
+        return paymentData;
+    }
+
+    public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (isIdEqual(savedPayment.getId(), id)) {
+                return savedPayment;
+            }
+        }
+        return null;
+    }
+
+    public Boolean isIdEqual(String id1, String id2){
+        if (id1.equals(id2)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
